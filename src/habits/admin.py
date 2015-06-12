6abd6a48fa44f67ca38adf8342cc7c1aaacfadc4
Max Shkurygin <max.atreides@gmail.com>
2015-06-12 16:07:59 -0400

    More user friendly admin display.

--- src/habits/admin.py
+++ src/habits/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Habit
 
-# Register your models here.
-admin.site.register(Habit)
+
+class HabitAdmin(admin.ModelAdmin):
+    fields = ['title', 'id', 'status']
+    list_display = ('created', 'title', 'id')
+
+
+admin.site.register(Habit, HabitAdmin)
