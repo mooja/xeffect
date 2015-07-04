9738b2d02738d73473622d0e0d1e07324da4ceaf
Max Shkurygin <max.atreides@gmail.com>
2015-07-04 09:40:10 -0400

    added basic styling to the homepage and the habit detail view

---
+++ src/static/site/js/dist/helloworld.js
@@ -0,0 +1,6 @@
+var i = React.createElement("h1", null, " i ");
+console.log('hello world!');
+console.log('two!');
+console.log('three!');
+console.log('four!');
+console.log('five!');
---
+++ src/static/site/js/src/helloworld.jsx
@@ -0,0 +1,6 @@
+var i = <h1> i </h1>;
+console.log('hello world!');
+console.log('two!');
+console.log('three!');
+console.log('four!');
+console.log('five!');
